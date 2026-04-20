ad331aec048e4780873253b535447dced82ff722
Favour Evans <divfavour04@gmail.com>
2026-04-20 22:52:03 +0100

    feat: Defined TypeScript types for invoices including status, address, and items.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello world, i am gonna create an invoice management app in 4 days time</h1>
+      <h2>let's fucking goooooooooooo</h2>
+    </>
+  )
+}
+
+export default App
