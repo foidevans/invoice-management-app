7de702aa11adf03b40a9f79a32f3a9ab3217fd8e
Favour Evans <divfavour04@gmail.com>
2026-04-22 23:26:52 +0100

    feat(router): add invoice routes and wrap app with context providers

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,28 @@ import ReactDOM from "react-dom/client";
 import "./styles/global.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { InvoiceProvider } from "./context/InvoiceContext";
+import InvoicesPage from "./pages/InvoicePage";
+import InvoiceDetailPage from './pages/InvoiceDetailPage'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
+   {
+    path: '/',
     element: <App />,
+    children: [
+      { index: true, element: <InvoicesPage /> },
+      { path: 'invoices/:id', element: <InvoiceDetailPage /> },
+    ],
   },
-]);
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <InvoiceProvider>
+        <RouterProvider router={router} />
+      </InvoiceProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
